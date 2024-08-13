5e4d002193e0238537cd942e1c15fe8bda8281f1
lichanghui <lichanghui1366768@gmail.com>
2024-08-13 22:30:30 +0800

    lodash

--- lodash/lichanghui11-lodash.js
+++ lodash/lichanghui11-lodash.js
@@ -35,6 +35,71 @@ var lichanghui11 = {
         return array;
     },
 
+    fill (array, value, start = 0, end = array.length) {
+        for (let i = start; i < end; i++) {
+            array[i] = value;
+        }
+    },
+
+    drop (array, num = 1) {
+        for (let i = 0; i < num; i++) {
+            if (array) array.shift();
+        } 
+        return array;
+    },
+
+    findIndex (array, fn, fromIndex = 0) {
+        for (let i = fromIndex; i < array.length; i++) {
+            if (fn (array[i])) {
+                return i;
+            } else {
+                return -1;
+            }
+        }
+    }, 
+
+    findLastIndex (array, fn, from = array.length - 1) {
+        for (let i = from; i > -1; i--) {
+            if (fn(array[i])) {
+                return i;
+            } else {
+                return -1;
+            }
+        }
+    },
+
+    flatten (array) {
+        let res = [];
+        for (let item of array) {
+            if (Array.isArray(item)) {
+                for (let it of item) {
+                    res.push(it);
+                }
+            } else {
+                res.push(item);
+            }
+       }
+        return res;
+    },
+
+    flattenDeep : function flattenDeep (array) {
+        let res = [];
+        for (let item of array) {
+            if (Array.isArray(item)) {
+                for (let it of flattenDeep(item)) {
+                    res.push(it);
+                }
+            } else {
+                res.push(item);
+            }
+        }
+        return res;
+    }, 
+
+
+
+
+
     difference(array, values) {
         var res = [];
         for (var i = 0; i < array.length; i++) {
@@ -56,7 +121,9 @@ var lichanghui11 = {
         var res = new MySet1(args.map(fn));
         
         return array.map(fn).filter(item => !res.has(item));
-    }
+    }, 
+
+
 }
 
 class MySet1 {
