01dcf188c9ecaa50720078da100451a2a6f0673a
lichanghui <lichanghui1366768@gmail.com>
2024-08-13 20:33:34 +0800

    lodash

--- lodash/lichanghui11-lodash.js
+++ lodash/lichanghui11-lodash.js
@@ -1,13 +1,40 @@
-function chunk (array, size = 1) {
-    let results = [];
-    let res = [];
-    for (let i = 0; i < array.length; i++) {
-        res.push(array[i]);
-        if (res.length === size) {
-            results.push(res); 
-            res = [];
+var lichanghui11 = {
+    chunk(array, size = 1) {
+        var results = [];
+        var res = [];
+        for (var i = 0; i < array.length; i++) {
+            res.push(array[i]);
+            if (res.length === size) {
+                results.push(res);
+                res = [];
+            }
         }
+        if (res.length) results.push(res);
+        return results;
+    },
+
+    compact(array) {
+        var res = [];
+        for (var item of array) {
+            if (item !== false) {
+                res.push(item);
+            }
+        }
+        return res;
+    },
+
+    concat(array, ...arguments) {
+        for (var i = 0; i < arguments.length; i++) {
+            if (Array.isArray(arguments[i])) {
+                array.push(...arguments[i]);
+            } else {
+                array.push(arguments[i]);
+            }
+        }
+        return array;
+    },
+
+    difference() {
+
     }
-    if (res.length) results.push(res);
-    return results;
 }
