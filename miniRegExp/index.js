ef4c49a967e86176c1200334298769b652e5147f
lichanghui <lichanghui1366768@gmail.com>
2024-11-24 21:47:23 +0800

    Conway

--- miniRegExp/index.js
+++ miniRegExp/index.js
@@ -1,5 +1,3 @@
-let button = document.getElementById('submit');
-let reInput = document.getElementById('reInput');
 /*
 reInput.addEventListener('keyup', function run() {
   let flag = getFlags();
@@ -35,41 +33,45 @@ reInput.addEventListener('keyup', function run() {
 
 })
   */
-button.addEventListener('click', function run() {
+let button = document.getElementById('submit');
+let reInput = document.getElementById('reInput');
+let checking = document.querySelector('span.checking');
+
+button.addEventListener('click', run);
+reInput.addEventListener('keyup', run);
+function run() {
+
   let flag = getFlags();
   let reString = reInput.value;
-  if (!reString) return;
-  try {
-    var re = new RegExp(reString, flag);
-  } catch (e) {
-    if (e instanceof TypeError) {
-      tips.innerText = e.message;
-      return;
-    } else {
-      throw e;
+  let re = new RegExp(reString, flag);
+  let testString = document.querySelector('#testInput').value;
+  setTimeout(() => {
+    checking.textContent = 'processing';
+  }, 1000);
+  let done = false;
+  getMatchesFromWorker(re, testString, (matches) => {
+    
+    checking.textContent = 'done';
+    let html = '';
+    let lastLastIndex = 0;
+    var matchIndex = 0;
+    for (let match of matches) {
+      html += testString.slice(lastLastIndex, match.index);
+      html += hightLightMatch(match, matchIndex);
+      lastLastIndex = match.index + match.length;
     }
-  }
-  let testString = testInput.value;
-  let html = '';
-  let lastLastIndex = 0;
-  var matchIndex = 0; 
-  while (match = re.exec(testString, flag)) {
-    html += testString.slice(lastLastIndex, match.index);
-    html += hightLightMatch(match, matchIndex);
-    lastLastIndex = re.lastIndex;
-    if (!match[0].length) re.lastIndex++;
-    if (re.global === false) break;
-  }
-  html += testString.slice(lastLastIndex);
+    html += testString.slice(lastLastIndex);
 
-  let result = document.getElementById('result');
-  result.innerHTML = html;
-  tips.innerText = '这里是错误提示！';
-})
+    let result = document.getElementById('result');
+    result.innerHTML = html;
+  }, e => {
+    checking.textContent = 'timeout';
+  })
+}
 
-let flags = ['i', 'g', 'm', 'd', 's', 'y', 'u'];
 
 function getFlags() {
+  let flags = ['i', 'g', 'm', 'd', 's', 'y', 'u'];
   return flags.filter(item => {
     let flag = document.getElementById('flag-' + item);
     if (flag.checked) return true;
@@ -83,14 +85,9 @@ let result1 = document.getElementById('result');
 textarea.addEventListener('scroll', function () {
   result1.scrollTop = textarea.scrollTop;
 })
-
-
-
 result1.addEventListener('scroll', function () {
   textarea.scrollTop = result1.scrollTop;
 });
-
-
 function hightLightMatch(match, matchIndex) {
   if (match[0].length === 0) {
     let info = [
@@ -102,7 +99,8 @@ function hightLightMatch(match, matchIndex) {
     return `<b class="zero-width-assertion" title="${info}"></b>`;
   }
   let array = new Array(match[0].length + 1).fill('');
-  let k = 0; 
+  let k = 0;
+  //将标签放入正确的位置； 
   for (let item of match.indices) {
     let i = item[0] - match.index, j = item[1] - match.index;
     let info = [
@@ -111,16 +109,40 @@ function hightLightMatch(match, matchIndex) {
       `Group ${k}: ${match[i]}`,
       `Pos: ${i}-${j}`,
     ].join('\n');
-    array[i] += `<b data-group-index="${k}" title="${info}">`; 
+    array[i] += `<b data-group-index="${k}" title="${info}">`;
     array[j] = array[j] + `</b>`;
     k++
     matchIndex++;
   }
   let res = '';
+  //将match中的每个字母放到正确的位置；
   for (let i = 0; i < match[0].length; i++) {
-    res += array[i]; 
+    res += array[i];
     res += match[0][i];
   }
   res += array[array.length - 1];
   return res;
+}
+
+
+function getMatchesFromWorker(re, string, func1, func2) {
+  let worker1 = new Worker('worker1.js');
+  let successed = false;
+  let matches;
+  worker1.addEventListener('message', (e) => {
+    matches = e.data;
+    worker1.terminate();
+    successed = true;
+  });
+  worker1.postMessage({
+    re: re, 
+    string: string
+  })
+  func1(matches);
+  if (!successed) {
+    setTimeout(() => {
+      func2() 
+      worker1.terminate();
+    }, 4000);
+  }
 }
---
+++ miniRegExp3/index.js
@@ -0,0 +1,6 @@
+// 取得元素DOM;
+let testBtn = document.querySelector('.test'); //测试按钮；
+let inputReg = document.querySelector('#inputReg');//正则输入；
+let output = document.querySelector('.result'); //字符串输入与输出；
+
+
